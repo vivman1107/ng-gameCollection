825b321c51038c45724c3e454e239f00ad2c0153
Vivekananda Maharaj <vivman1107@gmail.com>
2017-05-13 15:20:21 -0400

    Added sort functionality to game list

--- app/scripts/controllers/gameDisplay.js
+++ app/scripts/controllers/gameDisplay.js
@@ -2,10 +2,13 @@
 
 angular.module('gameCollectionApp')
     .controller('GameDisplayController', function($scope, GameService) {
-        $scope.gameList = [];
+        $scope.gameList = []; // set an empty array as the default game list
+        $scope.sortType     = 'name'; // set the default sort type
+        $scope.sortReverse  = false;  // set the default sort order
 
+        // Call the get games function in the game service to get all games
         GameService.getGames()
             .then(function(response) {
-                $scope.gameList = response;
+                $scope.gameList = response; // set game list to the response from get games
             });
     });
