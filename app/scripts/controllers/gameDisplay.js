274c4f78662f6fb9587c0ab160483e5b0aef2d02
Vivekananda Maharaj <vivman1107@gmail.com>
2017-05-25 13:50:24 -0400

    Added update and delete functionality

--- app/scripts/controllers/gameDisplay.js
+++ app/scripts/controllers/gameDisplay.js
@@ -1,14 +1,38 @@
 'use strict';
 
 angular.module('gameCollectionApp')
-    .controller('GameDisplayController', function($scope, GameService) {
+    .controller('GameDisplayController', function($scope, GameService, Notification) {
         $scope.gameList = []; // set an empty array as the default game list
         $scope.sortType     = 'name'; // set the default sort type
         $scope.sortReverse  = false;  // set the default sort order
 
-        // Call the get games function in the game service to get all games
-        GameService.getGames()
-            .then(function(response) {
-                $scope.gameList = response; // set game list to the response from get games
-            });
+        // Function to call the get games function in the game service to get all games
+        $scope.updateGameList = function() {
+            GameService.getGames()
+                .then(function(response) {
+                    $scope.gameList = response; // set game list to the response from get games
+                });
+        };
+
+        // Function to delete a game
+        $scope.deleteGame = function(gameID) {
+            GameService.deleteGame(gameID)
+                .then(function(response) {
+                    if (response.message === 'Successfully deleted') {
+                        $scope.updateGameList();
+                        Notification.success('Game deleted!');
+                    }
+                    else {
+                        Notification.error('An error occurred!');
+                    }
+                });
+        };
+        
+        // Update the game list when the update game list event is emitted or broadcasted
+        $scope.$on('updateGameList', function() {
+            $scope.updateGameList();
+        });
+
+        // Update the game list on controller load
+        $scope.updateGameList();
     });
--- app/scripts/services/game.js
+++ app/scripts/services/game.js
@@ -22,5 +22,32 @@ angular.module('gameCollectionApp')
                 });
         };
 
+        gameSvc.getGame = function(gameID) {
+            return $http.get('http://localhost:8080/api/games/' + gameID)
+                .then(function(response) {
+                    return response.data;
+                }, function(response) {
+                    console.log(response);
+                });
+        };
+
+        gameSvc.updateGame = function(data) {
+            return $http.put('http://localhost:8080/api/games/' + data._id, data)
+                .then(function(response) {
+                    return response.data;
+                }, function(response) {
+                    console.log(response);
+                });
+        };
+
+        gameSvc.deleteGame = function(gameID) {
+            return $http.delete('http://localhost:8080/api/games/' + gameID)
+                .then(function(response) {
+                    return response.data;
+                }, function(response) {
+                    console.log(response);
+                });
+        };
+
         return gameSvc;
     });
